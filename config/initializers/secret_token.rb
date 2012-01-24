d8a06343526d04337e731c85d4fded64f6622137
Mihai Frisan <mihai.frisan@sparktech.ro>
2012-01-24 13:10:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Priceput::Application.config.secret_token = '3248b955adf2c18b75af439a2cf59fab15d8bb289a89bdab79cfea5e50bd64739ce309ddde2630cfca5a7a682bf9562183b31cd2573b7adcc7775da8d3cd32dd'
