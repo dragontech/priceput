57589bcfd758ad3a270050bbf70865ea7f8af5dc
Mir <mirosul@gmail.com>
2012-02-10 12:23:32 +0200

    moved user specs where they belong

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,52 +1,5 @@
 require 'spec_helper'
 
-describe "User creation" do 
-  before (:each) do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
-    @user_attr = Factory.attributes_for(:user)
-  end
-  
-  it "successfully signed up" do
-    user = User.new(:email => @user_attr[:email])
-    user.password = @user_attr[:password]
-    user.password_confirmation = @user_attr[:password]
-    user.save
-    
-    user.should be_valid
-  end  
-
-  it "signed up without specifying the email" do
-    user = User.new(:email => "")
-    user.password = @user_attr[:password]
-    user.password_confirmation = @user_attr[:password]
-    user.save
-    
-    user.should have(1).error_on(:email)
-    user.errors_on(:email).should == ["can't be blank"]
-  end  
-
-  it "signed up without specifying the password" do
-    user = User.new(:email => @user_attr[:email])
-    user.password = ""
-    user.password_confirmation = ""
-    user.save
-    
-    user.should have(1).error_on(:password)
-    user.errors_on(:password).should == ["can't be blank"]
-  end  
-
-  it "signed up passwords dont match" do
-    user = User.new(:email => @user_attr[:email])
-    user.password = @user_attr[:password]
-    user.password_confirmation = "something else"
-    user.save
-    
-    user.should have(1).error_on(:password)
-    user.errors_on(:password).should == ["doesn't match confirmation"]
-  end  
-end
-
-
 describe "User" do 
   before (:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
