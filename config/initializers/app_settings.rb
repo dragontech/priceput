e4c0794a41aa89c03c1918951800154f0e31704d
Mir <mirosul@gmail.com>
2012-03-12 12:12:45 +0200

    judete

---
+++ config/initializers/app_settings.rb
@@ -0,0 +1,9 @@
+COUNTIES = ["Alba Iulia", "Alexandria", "Arad", "Bacau", "Baia Mare", "Barlad", 
+"Bistrita", "Botosani", "Braila", "Brasov", "Bucuresti", "Buzau", 
+"Calarasi", "Cluj Napoca", "Constanta", "Craiova", "Deva", "Drobeta Tr Severin",
+"Focsani", "Galati", "Giurgiu", "Hunedoara", "Iasi", "Miercurea Ciuc", 
+"Oradea", "Petrosani", "Piatra Neamt", "Pitesti", "Ploiesti", "Ramnicu Valcea",
+"Resita", "Roman", "Satu Mare", "Sfantu Gheorghe", "Sibiu", "Slatina", 
+"Slobozia", "Suceava", "Targoviste", "Targu Jiu", "Targu Mures", "Timisoara",
+"Tulcea", "Turda", "Vaslui", "Zalau"]
+
