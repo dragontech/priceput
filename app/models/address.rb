cb8e64dfece75539ebbe850733def61a688fa738
Mir <mirosul@gmail.com>
2012-03-12 12:12:35 +0200

    adaugare adrese

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,9 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
   
   validates :county, :city, :street, :number, presence: true
-  
+  validate :check_county
+
+  def check_county
+    errors[:base] << "This is not a Romanian county" unless COUNTIES.include?(county)
+  end
 end
